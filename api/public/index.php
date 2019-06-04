4a2ac00ead3fcee83949f19e11d890134e8b44f3
Vlad Duplin <vlad.duplin@ya.ru>
2019-06-04 19:53:31 +0300

    Add a simple Slim app

--- api/public/index.php
+++ api/public/index.php
@@ -2,9 +2,24 @@
 
 declare(strict_types=1);
 
-header('Content-Type: application/json');
+use Slim\Http\Request;
+use Slim\Http\Response;
 
-echo json_encode([
-    'name' => 'API app',
-    'version' => '1.0',
-]);
+require_once dirname(__DIR__ ) . '/vendor/autoload.php';
+
+$config = [
+    'settings' => [
+        'addContentLengthHeader' => false,
+    ]
+];
+
+$app = new \Slim\App($config);
+
+$app->get('/', function (Request $request, Response $response) {
+    return $response->withJson([
+        'name' => 'API app',
+        'version' => '1.0',
+    ]);
+});
+
+$app->run();
