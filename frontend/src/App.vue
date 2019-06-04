21919300c93d617679c1b35b5d79f1855c796a1f
Vlad Duplin <vlad.duplin@ya.ru>
2019-06-04 17:19:38 +0300

    Include bootstrap and fontawesome in package.json

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -30,6 +30,10 @@
 </script>
 
 <style lang="scss">
+    @import '~bootstrap/dist/css/bootstrap.css';
+    @import '~bootstrap-vue/dist/bootstrap-vue.css';
+    @import '~@fortawesome/fontawesome-free/css/all.css';
+
     body {
         max-height: 100vh;
     }
