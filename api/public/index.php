6320dc6b42dbb34fdd8c47bd3032f6fd2b6d30e7
Vlad Duplin <vlad.duplin@ya.ru>
2019-06-05 12:33:51 +0300

    Add .env in an app API

--- api/public/index.php
+++ api/public/index.php
@@ -3,9 +3,14 @@
 declare(strict_types=1);
 
 use Slim\App;
+use Symfony\Component\Dotenv\Dotenv;
 
 chdir(dirname(__DIR__));
 require_once 'vendor/autoload.php';
+
+if (file_exists('.env')) {
+    (new Dotenv())->load('.env');
+}
 
 (function () {
     $config = require_once 'config/config.php';
