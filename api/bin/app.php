8c503857d8f0704012f750843f03c7df947efe7a
Vlad Duplin <vlad.duplin@ya.ru>
2019-06-06 14:03:50 +0300

    In console application add migration commands

--- api/bin/app.php
+++ api/bin/app.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
+use Doctrine\DBAL\Migrations\Configuration\Configuration;
+use Doctrine\DBAL\Migrations\Tools\Console\Helper\ConfigurationHelper;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Tools\Console\ConsoleRunner;
 use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
@@ -19,10 +20,19 @@ if (file_exists('.env')) {
 $container = require_once 'config/container.php';
 $cli = new Application('Console application');
 
+/** @var EntityManagerInterface $entityManager */
 $entityManager = $container->get(EntityManagerInterface::class);
-$cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
+$connection = $entityManager->getConnection();
 
-ConsoleRunner::addCommands($cli);
+$configuration = new Configuration($connection);
+$configuration->setMigrationsDirectory('src/Data/Migration');
+$configuration->setMigrationsNamespace('Api\Data\Migration');
+
+$cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
+$cli->getHelperSet()->set(new ConfigurationHelper($connection, $configuration), 'configuration');
+
+\Doctrine\ORM\Tools\Console\ConsoleRunner::addCommands($cli);
+\Doctrine\DBAL\Migrations\Tools\Console\ConsoleRunner::addCommands($cli);
 
 $commands = $container->get('config')['console']['commands'];
 foreach ($commands as $command) {
