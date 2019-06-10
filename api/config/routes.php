d38464eff67af741d967dc75ab637e38df3a4d37
Vlad Duplin <vlad.duplin@ya.ru>
2019-06-10 16:24:21 +0300

    Add controller is sign up

--- api/config/routes.php
+++ api/config/routes.php
@@ -1,8 +1,10 @@
 <?php
 
+use Api\Http\Action\Auth\SignUp\RequestAction;
 use Api\Http\Action\HomeAction;
 use Slim\App;
 
 return function (App $app) {
     $app->get('/', HomeAction::class . ':handle');
+    $app->post('/auth/signup', RequestAction::class . ':handle');
 };
