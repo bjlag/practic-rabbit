03dc68242a6656bde62684cbbbd67a1b7c0d48b8
Vlad Duplin <vlad.duplin@ya.ru>
2019-06-11 19:50:30 +0300

    Add mailer

---
+++ api/config/common/mailer.php
@@ -0,0 +1,25 @@
+<?php
+
+use Psr\Container\ContainerInterface;
+
+return [
+    'config' => [
+        'mailer' => [
+            'host' => getenv('APP_MAILER_HOST'),
+            'port' => (int) getenv('APP_MAILER_PORT'),
+            'username' => getenv('APP_MAILER_USERNAME'),
+            'password' => getenv('APP_MAILER_PASSWORD'),
+            'encryption' => getenv('APP_MAILER_ENCRYPTION'),
+        ]
+    ],
+
+    Swift_Mailer::class => function (ContainerInterface $container) {
+        $config = $container->get('config')['mailer'];
+
+        $transport = (new Swift_SmtpTransport($config['host'], $config['port'], $config['encryption']))
+            ->setUsername($config['username'])
+            ->setPassword($config['password']);
+
+        return new Swift_Mailer($transport);
+    }
+];
