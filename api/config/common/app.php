6052314e7115a7048e0990b922143e5ad5b548f3
Vlad Duplin <vlad.duplin@ya.ru>
2019-06-15 16:41:00 +0300

    Improvement formatting

--- api/config/common/app.php
+++ api/config/common/app.php
@@ -1,9 +1,12 @@
 <?php
 
-use Api\Http\Action;
+use Api\Http\Middleware;
+use Api\Http\Validator\SymfonyValidator;
+use Api\Infrastructure\Model as ModelInfrastructure;
 use Api\Model;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Container\ContainerInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 return [
     'config' => [
@@ -12,7 +15,7 @@ return [
         ]
     ],
 
-    \Symfony\Component\Validator\Validator\ValidatorInterface::class => function () {
+    ValidatorInterface::class => function () {
         \Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
 
         return Symfony\Component\Validator\Validation::createValidatorBuilder()
@@ -20,9 +23,9 @@ return [
             ->getValidator();
     },
 
-    \Api\Http\Validator\SymfonyValidator::class => function(ContainerInterface $container) {
-        return new \Api\Http\Validator\SymfonyValidator(
-            $container->get(\Symfony\Component\Validator\Validator\ValidatorInterface::class)
+    SymfonyValidator::class => function (ContainerInterface $container) {
+        return new SymfonyValidator(
+            $container->get(ValidatorInterface::class)
         );
     },
 
@@ -34,29 +37,29 @@ return [
         return new Middleware\DomainExceptionMiddleware();
     },
 
-    \Api\Http\Middleware\ValidationExceptionMiddleware::class => function () {
-        return new \Api\Http\Middleware\ValidationExceptionMiddleware();
+    Middleware\ValidationExceptionMiddleware::class => function () {
+        return new Middleware\ValidationExceptionMiddleware();
     },
 
     Model\User\Service\Flusher::class => function (ContainerInterface $container) {
-        return new \Api\Infrastructure\Model\Service\DoctrineFlusher(
+        return new ModelInfrastructure\Service\DoctrineFlusher(
             $container->get(EntityManagerInterface::class),
             $container->get(Model\EventDispatcher::class)
         );
     },
 
     Model\User\Entity\UserRepository::class => function (ContainerInterface $container) {
-        return new \Api\Infrastructure\Model\User\Entity\DoctrineUserRepository(
+        return new ModelInfrastructure\User\Entity\DoctrineUserRepository(
             $container->get(EntityManagerInterface::class)
         );
     },
 
-    Model\User\Service\PasswordHasher::class => function (ContainerInterface $container) {
-        return new \Api\Infrastructure\Model\User\Service\BCryptPasswordHasher();
+    Model\User\Service\PasswordHasher::class => function () {
+        return new ModelInfrastructure\User\Service\BCryptPasswordHasher();
     },
 
     Model\User\Service\ConfirmTokenizer::class => function (ContainerInterface $container) {
-        return new \Api\Infrastructure\Model\User\Service\RandConfirmTokenizer(
+        return new ModelInfrastructure\User\Service\RandConfirmTokenizer(
             new DateInterval($container->get('config')['auth']['confirm_token_interval'])
         );
     },
